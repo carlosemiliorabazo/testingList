a586c9f7a101e4bfb7c310c2cc42d731be5eff8f
Carlos Rabazo <carlosemilio.rabazo@ufv.es>
2022-10-29 08:02:29 +0200

    Primer Commit

--- src/main/java/com/ufv/lists/Lists.java
+++ src/main/java/com/ufv/lists/Lists.java
@@ -26,7 +26,10 @@ public class Lists {
         this.list.removeIf(n -> elem == n);
     }
 
-    // Anotacion de sobreescritura
+    // Anotacion de sobreescritura. forzar al compilador a comprobar en tiempo de
+    // compilación que estás sobrescribiendo correctamente un método, y de este
+    // modo evitar errores en tiempo de ejecución, los cuales serían mucho más
+    // difíciles de detectar.
     @Override
     public String toString() {
         return "" + list + "";
